d2087e96c7aa4cc467c0739e722683215da41922
Deepak Cherian <deepak@cherian.net>
2025-01-17 16:10:27 -0700

    Update container

--- src/lib_modal.py
+++ src/lib_modal.py
@@ -10,8 +10,8 @@ MODAL_IMAGE = (
     Image.debian_slim(python_version="3.12")
     .apt_install("curl")
     .pip_install(
-        "arraylake>=0.13.1",
-        "icechunk >= 0.1.0a7",
+        "arraylake>=0.13.3",
+        "icechunk >= 0.1.0a12",
         "certifi",
         "cfgrib",
         "dask",
@@ -23,13 +23,13 @@ MODAL_IMAGE = (
         "eccodes==2.37",
         "pyproj",
     )
+    .add_local_python_source("src")
 )
 
 
 class ModalKwargs(TypedDict):
     image: modal.Image
     secrets: Sequence[modal.Secret]
-    mounts: Sequence[modal.Mount]
 
 
 MODAL_FUNCTION_KWARGS: ModalKwargs = dict(
@@ -38,5 +38,4 @@ MODAL_FUNCTION_KWARGS: ModalKwargs = dict(
         modal.Secret.from_name("ryan-aws-secret"),
         modal.Secret.from_name("deepak-arraylake-demos-token"),
     ],
-    mounts=[modal.Mount.from_local_python_packages("src")],
 )
