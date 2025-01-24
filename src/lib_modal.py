d0e32688b846bf6a7d6cf57ef5bb5bb1838ac6e9
Deepak Cherian <deepak@cherian.net>
2025-01-24 09:49:37 -0700

    bump image

--- src/lib_modal.py
+++ src/lib_modal.py
@@ -7,10 +7,10 @@ import modal
 from modal import Image
 
 MODAL_IMAGE = (
-    Image.debian_slim(python_version="3.11")
-    .apt_install("libglib2.0-dev", "curl")
+    Image.debian_slim(python_version="3.12")
+    .apt_install("curl")
     .pip_install(
-        "arraylake",
+        "arraylake>=0.13.3",
         "certifi",
         "cfgrib",
         "dask",
@@ -18,24 +18,17 @@ MODAL_IMAGE = (
         "herbie-data",
         "s3fs",
         "xarray",
-        "pydantic-core==2.18.2",
-        "pydantic==2.7.1",
         "fastapi>=0.108",
+        "eccodes==2.37",
+        "pyproj",
     )
-    .pip_install("eccodes", "ecmwflibs")
-    .env(
-        {
-            "SSL_CERT_FILE": "/opt/conda/lib/python3.11/site-packages/certifi/cacert.pem",
-        }
-    )
-    .run_commands("python -m eccodes selfcheck")
+    .add_local_python_source("src")
 )
 
 
 class ModalKwargs(TypedDict):
     image: modal.Image
     secrets: Sequence[modal.Secret]
-    mounts: Sequence[modal.Mount]
 
 
 MODAL_FUNCTION_KWARGS: ModalKwargs = dict(
@@ -44,5 +37,4 @@ MODAL_FUNCTION_KWARGS: ModalKwargs = dict(
         modal.Secret.from_name("ryan-aws-secret"),
         modal.Secret.from_name("deepak-arraylake-demos-token"),
     ],
-    mounts=[modal.Mount.from_local_python_packages("src")],
 )
