e7c89de5d77c0b2b7a0e0d178accd624c47c4bdc
Deepak Cherian <deepak@cherian.net>
2025-08-28 16:11:07 -0600

    tweaks

--- modal_hrrr.py
+++ modal_hrrr.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from datetime import datetime, timedelta
 
 import modal
@@ -6,7 +8,7 @@ from src.lib import ReadMode, WriteMode
 from src.lib_modal import MODAL_FUNCTION_KWARGS
 from src.modal_app import applib, driver
 
-app = modal.App("hrrr-ingest")
+app = modal.App("arraylake-hrrr")
 app.include(applib)  # necessary
 
 
@@ -20,16 +22,16 @@ def main(mode: str, toml_file: str, since: str, till: str | None = None):
 
 @app.function(**MODAL_FUNCTION_KWARGS, schedule=modal.Cron("*/30 * * * *"), timeout=300)
 def hrrr_verify():
-    driver(mode=ReadMode.VERIFY, toml_file_path="src/configs/hrrr.toml")
+    driver(mode=ReadMode.VERIFY, toml_file_path="src/configs/hrrr-integration.toml")
 
 
 @app.function(**MODAL_FUNCTION_KWARGS, timeout=3600 * 3)
 def hrrr_backfill():
     """Run this "backfill" function wtih `modal run modal_hrrr.py::hrrr_backfill`."""
-    file = "src/configs/hrrr.toml"
+    file = "src/configs/hrrr-integration.toml"
     mode = WriteMode.BACKFILL
-    since = datetime(2025, 8, 24)
-    till = datetime.now() - timedelta(days=1, hours=12)
+    since = datetime(2025, 8, 12)
+    till = datetime.now() - timedelta(days=1, hours=0)
     # till = datetime(2025, 2, 4, 0, 0, 0)
 
     driver(mode=mode, toml_file_path=file, since=since, till=till)
@@ -38,6 +40,6 @@ def hrrr_backfill():
 @app.function(**MODAL_FUNCTION_KWARGS, timeout=20 * 60, schedule=modal.Cron("57 * * * *"))
 def hrrr_update_solar():
     """Run this "backfill" function wtih `modal run modal_hrrr.py::hrrr_backfill`."""
-    file = "src/configs/hrrr.toml"
+    file = "src/configs/hrrr-integration.toml"
     mode = WriteMode.UPDATE
     driver(mode=mode, toml_file_path=file)
